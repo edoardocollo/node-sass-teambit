386a8931fa42133e76e6ec61fb91800578c57c32
edoardo <collo>
2021-01-26 16:07:52 +0100

    header and jumbotron structure

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,8 +13,48 @@ var app = new Vue({
     // HEADER DATA
     /////////////////
     header: {
-      logo: '',
-      link: ['solutions', 'pricing', 'blog', 'signe in', 'signe up']
+      logo: 'dist/img/logo.svg',
+      links: ['solutions', 'pricing', 'blog', 'signe in', 'signe up']
+    },
+    // JUMBOTRON DATA
+    /////////////////
+    jumbotron: {
+      title: 'Understand, recognize and develop your people',
+      text: 'Teambit helps teams create a high performing culture with regular feedback.',
+      links: ['sign up with google', 'sign up with slack', 'or use your email address']
+    },
+    // FIRST SECTION DATA
+    /////////////////
+    firstSection: {
+      title: 'Traditional people processes lead to diseingagement and turnover.',
+      card: [{
+        img: '',
+        text: 'They are hardcore, formal and disconnected'
+      }, {
+        img: '',
+        text: 'They don\'t encourage regular feedbacks'
+      }, {
+        img: '',
+        text: 'They dont\'t make it safe speak up'
+      }],
+      img: ''
+    },
+    // SECOND SECTION DATA
+    /////////////////
+    secondSection: {
+      title: 'It doesn\'t to be like that. Teambit is a modern solution that teammates love.',
+      card: [{
+        img: '',
+        text: 'it is lightweight fun and interconnected'
+      }, {
+        img: '',
+        text: 'It encourage regular feedbacks with contextual requests'
+      }, {
+        img: '',
+        text: 'It care safe envirnoment for dialog'
+      }],
+      img: '',
+      text: 'Here is how it works'
     }
   }
 });
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ let app = new Vue({
     // HEADER DATA
     /////////////////
     header:{
-      logo:'',
-      link:[
+      logo:'dist/img/logo.svg',
+      links:[
         'solutions',
         'pricing',
         'blog',
@@ -13,6 +13,60 @@ let app = new Vue({
         'signe up',
       ],
     },
-    
+    // JUMBOTRON DATA
+    /////////////////
+
+    jumbotron:{
+      title:'Understand, recognize and develop your people',
+      text:'Teambit helps teams create a high performing culture with regular feedback.',
+      links:[
+        'sign up with google',
+        'sign up with slack',
+        'or use your email address',
+      ],
+    },
+    // FIRST SECTION DATA
+    /////////////////
+
+    firstSection:{
+      title:'Traditional people processes lead to diseingagement and turnover.',
+      card:[
+        {
+          img:'',
+          text:'They are hardcore, formal and disconnected'
+        },
+        {
+          img:'',
+          text:'They don\'t encourage regular feedbacks'
+        },
+        {
+          img:'',
+          text:'They dont\'t make it safe speak up',
+        },
+      ],
+      img:'',
+    },
+    // SECOND SECTION DATA
+    /////////////////
+
+    secondSection:{
+      title:'It doesn\'t to be like that. Teambit is a modern solution that teammates love.',
+      card:[
+        {
+          img:'',
+          text:'it is lightweight fun and interconnected'
+        },
+        {
+          img:'',
+          text:'It encourage regular feedbacks with contextual requests'
+        },
+        {
+          img:'',
+          text:'It care safe envirnoment for dialog',
+        },
+      ],
+      img:'',
+      text:'Here is how it works',
+    },
   },
 });

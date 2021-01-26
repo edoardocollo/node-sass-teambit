6d2ee50a7a4b5b09784362c6d370be9225428d8c
edoardo <collo>
2021-01-26 15:43:42 +0100

    starting structure

---
+++ src/main.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+  el:'#app',
+  data:{
+    // HEADER DATA
+    /////////////////
+    header:{
+      logo:'',
+      link:[
+        'solutions',
+        'pricing',
+        'blog',
+        'signe in',
+        'signe up',
+      ],
+    },
+    
+  },
+});
